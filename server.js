ed1e5042329ab62ef88e3df45926ad69b6c9467f
David Eliason <david@davethemaker.com>
2018-07-03 18:21:48 -0700

    create schema and model and attach a method to the schema

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ dotenv.config();
 var app = express();
 
 var index = require('./routes/index');
+app.use('/', index);
 
+// Mongoose and mLab Time!
 const MLAB_URI = process.env.MLAB_URI;
 var mongoose = require('mongoose');
 mongoose.connect(MLAB_URI);
@@ -19,9 +21,27 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log("we are connected to DB");
+
+    var kittySchema = mongoose.Schema({
+        name: String
+    });
+
+    kittySchema.methods.speak = function () {
+        var greeting = this.name ? "Hi my name is " + this.name : "meow meow";
+        console.log(greeting);
+    }
+    var Kitten = mongoose.model('Kitten', kittySchema);
+    var silence = new Kitten({
+        name: "Silence"
+    });
+    silence.speak();
+
+
+
+
+
 });
 
-app.use('/', index);
 // app.get('/addfriend', friends.addFriend);
 var listener = app.listen(8888, function () {
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
